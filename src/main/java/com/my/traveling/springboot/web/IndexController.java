c48c38b3b1185b89526a6bb399d41289a378478d
jinny <qkrdmehf@gmail.com>
2023-05-26 00:50:44 +0900

    Post 수정 기능 완성

--- src/main/java/com/my/traveling/springboot/web/IndexController.java
+++ src/main/java/com/my/traveling/springboot/web/IndexController.java
@@ -2,10 +2,12 @@ package com.my.traveling.springboot.web;
 
 
 import com.my.traveling.springboot.service.PostsService;
+import com.my.traveling.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import sun.applet.AppletViewer;
 
 @RequiredArgsConstructor
@@ -24,6 +26,14 @@ public class IndexController {
     public String postSave(){
         return "posts-save";
     }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+        return "posts-update";
+    }
+
 
     @GetMapping("/main")
     public String main(){
