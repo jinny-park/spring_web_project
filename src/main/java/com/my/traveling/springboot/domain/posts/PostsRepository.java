53870025aee6a388f2be827113a5a33b1c9147bb
jinny <qkrdmehf@gmail.com>
2023-05-26 00:40:56 +0900

    PostsRepository 쿼리문 작성

--- src/main/java/com/my/traveling/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/my/traveling/springboot/domain/posts/PostsRepository.java
@@ -2,6 +2,12 @@ package com.my.traveling.springboot.domain.posts;
 
 import com.my.traveling.springboot.domain.posts.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC") // 스프링 데이터에서 제공하지 않는 메소드는 위처럼 쿼리로 작성 가능
+    List<Posts> findAllDesc();
 }
