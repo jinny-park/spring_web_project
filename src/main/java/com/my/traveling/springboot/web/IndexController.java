faac85c8588e28f1939ac0a2c13a4520a7f5a990
jinny <qkrdmehf@gmail.com>
2023-07-07 23:15:46 +0900

    IndexController SessionUser 추가

--- src/main/java/com/my/traveling/springboot/web/IndexController.java
+++ src/main/java/com/my/traveling/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.my.traveling.springboot.web;
 
 
+import com.my.traveling.springboot.config.auth.dto.SessionUser;
 import com.my.traveling.springboot.service.PostsService;
 import com.my.traveling.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -10,15 +11,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import sun.applet.AppletViewer;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
-    @GetMapping("/index")
+    @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user!=null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
